d46e6c34884fa00e78fa36323000721e739fbf79
YangJ00n <yangjoon820@gmail.com>
2022-05-30 07:51:35 +0900

    Setup

---
+++ README.md
@@ -0,0 +1,5 @@
+# KI-AMS
+
+Busan <b>K</b>yong<b>i</b>l High School <b>A</b>ttendance <b>M</b>anagement <b>S</b>ervice 📝
+
+부산 경일고등학교 야간 자율학습 출석 관리 서비스
---
+++ src/server.js
@@ -0,0 +1,15 @@
+import express from "express";
+import morgan from "morgan";
+
+const app = express();
+const logger = morgan("dev");
+
+app.set("view engine", "pug");
+app.set("views", __dirname + "/views");
+
+app.use(logger);
+app.use("/static", express.static("assets"));
+
+app.get("/", (_, res) => res.render("home"));
+
+export default app;
